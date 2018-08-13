147285c636c6d5d2bb0ac5442002856e803dbecf
liriodb <liriors2.0@gmail.com>
2018-08-13 12:50:27 +0200

    directivas

--- src/app/confirmation.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ConfirmationDirective } from './confirmation.directive';
-
-describe('ConfirmationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ConfirmationDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/confirmation.directive.ts
+++ src/app/confirmation.directive.ts
@@ -1,10 +1,18 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appConfirmation]'
 })
+
 export class ConfirmationDirective {
+  @Input('confirm') execFunction: Function;
+  
+  @HostListener('click', ['$event'])
+  onclick(){
+    const confirmed = window.confirm('¿Estas seguro de querer guardar?')
 
-  constructor() { }
-
+    if(confirmed){
+      this.execFunction();
+    }
+  }
 }
